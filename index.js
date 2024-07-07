d4d831e20af3d3b21f176b89108dcaa4e1d95f56
Arif Hossain <arif.raj9911@gmail.com>
2024-07-07 18:04:11 +0600

    connect mongodb with express and get api added

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// arifraj9911
+// 3MAjkVdYRKYEC5LO
+
+const uri =
+  "mongodb+srv://arifraj9911:3MAjkVdYRKYEC5LO@cluster0.byhl9o0.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const chatCollection = client.db("dbChat").collection("chat");
+
+    app.get("/chats", async (req, res) => {
+      const result = await chatCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Telegram replica server running");
