3f8b9b0a2df839b65701d254ee5dc156c74ce028
Arif Hossain <arif.raj9911@gmail.com>
2024-07-07 23:28:16 +0600

    client server live link added to the cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors({
   origin: [
-    "http://localhost:5173",
+    "http://localhost:5173","https://telegram-replica.vercel.app"
   ]
 }));
 
