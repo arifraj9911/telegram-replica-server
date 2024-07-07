6c899c2c46a736164ba15fe64c4969cd531085d4
Arif Hossain <arif.raj9911@gmail.com>
2024-07-07 23:24:10 +0600

    vercel deployment code added

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+  ]
+}));
 
 // arifraj9911
 // 3MAjkVdYRKYEC5LO
@@ -27,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const chatCollection = client.db("dbChat").collection("chat");
 
@@ -63,7 +67,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
